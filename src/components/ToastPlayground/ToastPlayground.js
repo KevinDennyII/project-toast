96854075a6e6e4ef9b3f358e0edc14ae330a1cbd
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-05-11 02:14:47 -0500

    updated implemention for capturing state for selected toast option

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -7,6 +7,20 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  // capture state of toast messaging
+  const [message, setMessage] = React.useState('')
+  const [toastOption, setSelectedToastOption] = React.useState('');
+
+  function handleChange(event){
+    setMessage(event.target.value);
+    console.log(message)
+  }
+
+  function handleSelectedOption(event){
+    setSelectedToastOption(event.target.value);
+    console.log(toastOption);
+  }
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -24,7 +38,7 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea id="message" value={message} onChange={handleChange} className={styles.messageInput} />
           </div>
         </div>
 
@@ -34,12 +48,16 @@ function ToastPlayground() {
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
             { VARIANT_OPTIONS.map((option) => (
-                <label htmlFor={`variant-${option}`}>
+                <label key={`${option}-${Math.random()}`} htmlFor={`variant-${option}`}>
                 <input
                   id={`variant-${option}`}
+                  key={`${option}-${Math.random()/2}`}
                   type="radio"
                   name="variant"
                   value={`${option}`}
+                  // defaultChecked={toastOption === 'notice'}
+                  checked={toastOption === option}
+                  onChange={handleSelectedOption}
                 />
                   {option}
               </label>
